580ef62e8e68ee9c66fa6cad8acd2620d8328eb5
Alexandre Betassa de Souza <5by5.betassa@voeazul.com.br>
2025-03-30 12:03:39 -0300

    ajustado namespaces

---
+++ src/Api/IoC/Bootstrapper.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.User.Api.IoC
+{
+    public static class Bootstrapper
+    {
+        public static void InjectDepencies(this IServiceCollection services)
+        {
+
+        }
+    }
+}
